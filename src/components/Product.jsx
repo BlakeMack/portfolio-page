353c6a9daa945b648a7436d03c40f86391f3fcb0
Blake McKenzie <blakemckenzie365@gmail.com>
2023-07-26 11:56:02 +0100

    labelled product components with their relevant names, centered product componenents within the product list for responsiveness on all screen sizes

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,18 +2,24 @@ import React from 'react'
 import "./Product.css"
 import logo from "../img/logo192.png"
 
-const Product = ({img, link, name}) => {
+const Product = ({img, link, name, projectName}) => {
   return (
-    <div className='product'>
-      <div className="p-browser">
-        <div className="p-circle"></div>
-        <div className="p-circle"></div>
-        <div className="p-circle"></div>
+    <>
+    <div className='product-wrapper'>
+      <h1 className='p-name'>{projectName}</h1>
+      <div className='product'>
+        <div className="p-browser">
+          <div className="p-circle"></div>
+          <div className="p-circle"></div>
+          <div className="p-circle"></div>
+        </div>
+        <a href={link} target="_blank" rel="noreferrer">
+          <img src={img} alt="" className={name}/>
+        </a>
       </div>
-      <a href={link} target="_blank" rel="noreferrer">
-        <img src={img} alt="" className={name}/>
-      </a>
     </div>
+    </>
+
   )
 }
 
